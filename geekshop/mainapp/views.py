031882ea0a0b30ff68b7a7338c6fa37a6506f1a1
Roman <roman.motrich@yandex.ru>
2022-01-15 20:46:53 +0300

    Урок 2
    base.html + context start

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context={
+        'title':'Geekshop | Главная',
+    }
+    return render(request,'mainapp/index.html',context)
+
+def products(request):
+    return render(request,'mainapp/products.html')
