a7a1d886cb64a5b942cb407b921a845489092a0b
Roman <roman.motrich@yandex.ru>
2022-01-15 19:19:00 +0300

    Урок 1.
    Всё по уроку 1

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index,products
+from mainapp.views import products
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',index,name='index'),
+    # path('admin/', admin.site.urls),
+    #  path('',index,name='index'),
     path('products/',products,name='products'),
 ]
