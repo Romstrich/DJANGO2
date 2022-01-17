691332cc294014fd2b8b858fccd794e1073164f4
Roman <roman.motrich@yandex.ru>
2022-01-17 15:43:58 +0300

    Урок 3
    Новая миграция + __str___ для моделей продуктов и категорий

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 #Модель для категорий
-class Product_Category(models.Model):
+class ProductCategory(models.Model):
     name=models.CharField(max_length=64)
     description=models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.name},{self.description}'
 
 class Product(models.Model):
     name = models.CharField(max_length = 64)
@@ -13,6 +16,7 @@ class Product(models.Model):
     description = models.TextField(blank=True,null=True)
     price = models.DecimalField(max_digits=8,decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(Product_Category,on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory,on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return f'{self.name},{self.category},{self.description},{self.price}'
