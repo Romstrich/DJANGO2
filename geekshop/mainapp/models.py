6e638913565d150b7e6c22102bdcd1d19afa0d84
Roman <roman.motrich@yandex.ru>
2022-01-17 06:12:13 +0300

    Урок 3
    Создание моделей и миграций к ним (make migrations <имя приложения>)
    Product_Category
    Product
    makemigrations mainapp (если миграция в приложении создаваться не хочет)

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#Модель для категорий
+class Product_Category(models.Model):
+    name=models.CharField(max_length=64)
+    description=models.TextField(blank=True,null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length = 64)
+    image = models.ImageField(upload_to='product_image',blank=True)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Product_Category,on_delete=models.CASCADE)
+
+
