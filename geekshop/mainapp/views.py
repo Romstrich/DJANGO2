b7f8cb4b278dc9969c7db83a6dcb9bc35a9cfde8
Roman <roman.motrich@yandex.ru>
2022-01-17 04:08:58 +0300

    Урок 3
    Загрузка с json категории и товары

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,6 @@
+import json
+import os
+MODULE_DIR=os.path.dirname(__file__)
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,40 +13,14 @@ def index(request):
 
 def products(request):
     #load from file
-    product_list=[
-  {"name":"Худи черного цвета с монограммами adidas Originals",
-  "price":"6 090,00 руб.",
-  "description":"Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
-  "image":"vendor/img/products/Adidas-hoodie.png"},
-
-  {"name":"Синяя куртка The North Face",
-  "price":"23 725,00 руб.",
-  "description":"Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.",
-  "image":"vendor/img/products/Blue-jacket-The-North-Face.png"},
-
-  {"name":"Коричневый спортивный oversized-топ ASOS DESIGN",
-  "price":"3 390,00 руб.",
-  "description":"Материал с плюшевой текстурой. Удобный и мягкий.",
-  "image":"vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png"},
-
-  {"name":"Черный рюкзак Nike Heritage",
-  "price":"2 340,00 руб.",
-  "description":"Плотная ткань. Легкий материал.",
-  "image":"vendor/img/products/Black-Nike-Heritage-backpack.png"},
-
-  {"name":"Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex",
-  "price":"13 590,00 руб.",
-  "description":"Гладкий кожаный верх. Натуральный материал.",
-  "image":"/vendor/img/products/Black-Dr-Martens-shoes.png"},
-
-  {"name":"Темно-синие широкие строгие брюки ASOS DESIGN",
-  "price":"2 890,00 руб",
-  "description":"Легкая эластичная ткань сирсакер Фактурная ткань.",
-  "image":"vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png"}
-
-]
+    json_products=os.path.join(MODULE_DIR,'fixtures/products.json')
+    json_categories=os.path.join(MODULE_DIR,'fixtures/categories.json')
     context = {
         'title': 'Geekshop | Товары',
-        'products': product_list,
+        'products': json.load(open(json_products,encoding='utf-8')),
+        'categories':json.load(open(json_categories,encoding='utf-8')),
     }
+
+
+
     return render(request,'mainapp/products.html',context)
